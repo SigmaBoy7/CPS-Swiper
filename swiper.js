c6c5cabc1dd5960c5d5bcb8fd9251df8e2907402
Rahmidin Hasanov <rahmiddin.hasanov@bk.ru>
2024-02-02 18:46:47 +0500

    Fix img url2

---
+++ swiper.js
@@ -0,0 +1,27 @@
+import Swiper from './node_modules/swiper/swiper.min.mjs';
+import Pagination from './node_modules/swiper/modules/pagination.min.mjs';
+
+const swiper = new Swiper('.brand__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 'auto',
+    modules: [Pagination],
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
